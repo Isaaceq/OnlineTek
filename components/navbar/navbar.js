1fd141c4ff143284285529d32769c5a772ba75b6
Isaac Escobar <isaaceq25@gmail.com>
2017-10-04 18:23:12 -0700

    End of the day commit. Ready to work from desktop

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -4,4 +4,21 @@ app.directive('navbar', function () {
     restrict: 'E',
     templateUrl: '../components/navbar/navbar.html',
   };
+
+  // $(document).ready(function() {
+  //   $('#fullpage').fullpage({
+  //
+  //   //   //Navigation
+  //   // menu: '#menu',
+  //   // lockAnchors: false,
+  //   // anchors:['firstPage', 'secondPage'],
+  //   // navigation: false,
+  //   // navigationPosition: 'right',
+  //   // navigationTooltips: ['firstSlide', 'secondSlide'],
+  //   // showActiveTooltip: false,
+  //   // slidesNavigation: false,
+  //   // slidesNavPosition: 'bottom',
+  //   });
+  // });
+
 });
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,9 @@
 'use strict';
 app.controller("homeController", function($scope) {
 
+  $(document).ready(function() {
+  	$('#fullpage').fullpage();
+  });
+
+
   });
